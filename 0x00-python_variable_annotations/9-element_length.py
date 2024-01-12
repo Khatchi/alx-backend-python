191e557d5f9a5271daf716d836bcdd4fc5545efb
Khatchi <Khatchi>
2024-01-12 15:19:23 +0100

    task9: Doc added

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -1,17 +1,8 @@
 #!/usr/bin/env python3
-from typing import List, Tuple
+""" Module documentation """
+from typing import Iterable, Sequence, Tuple, List
 
 
-def element_length(lst: List[str]) -> List[Tuple[str, int]]:
-    """
-    Return a list of tuples, each containing an element from the input
-    list and its length.
-
-    Parameters:
-    - lst (List[str]): The input list of strings.
-
-    Returns:
-    - List[Tuple[str, int]]: A list of tuples where each tuple contains
-      a string from the input list and its length.
-    """
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    """Func doc"""
     return [(i, len(i)) for i in lst]
