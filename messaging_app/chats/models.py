8230172b7c771fe7bb5af997fce2f97f1e20d490
Khatchi <solomon9ice2014@gmail.com>
2025-05-31 23:34:40 +0100

    refactored the User, Conversation and Message models

--- messaging_app/chats/models.py
+++ messaging_app/chats/models.py
@@ -1,23 +1,36 @@
+import uuid
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 
 class User(AbstractUser):
-    """Custom user model extending Django's AbstractUser."""
-    bio = models.TextField(blank=True, null=True)
-    profile_picture = models.CharField(max_length=100, blank=True, null=True)
+    """Custom user model that extends Django's AbstractUser."""
+    user_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    first_name = models.CharField(max_length=150, blank=True)
+    last_name = models.CharField(max_length=150, blank=True)
+    email = models.EmailField(unique=True)
+    phone_number = models.CharField(max_length=15, blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         """Return a string representation of the user."""
-        # return self.username
-        return f"User: {self.username}" if self.username else "User: Anonymous"
+        return f"User: {self.username} (ID: {self.user_id})"\
+        if self.username else "User: Anonymous"
 
+    class Meta:
+        """Meta options for the User model."""
+        ordering = ['-created_at']
+        indexes = [
+            models.Index(fields=['username'], name='idx_user_username'),
+            models.Index(fields=['email'], name='idx_user_email'),
+        ]
 
 class Conversation(models.Model):
     """Model representing a conversation between users."""
+    participants = models.ManyToManyField(User, related_name='conversations')
     title = models.CharField(max_length=100, blank=True, null=True)
-    users = models.ManyToManyField(User, related_name='conversations')
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
@@ -32,7 +45,7 @@ class Message(models.Model):
     """Model representing a message in a conversation."""
     conversation = models.ForeignKey(Conversation, related_name='messages', on_delete=models.CASCADE)
     sender = models.ForeignKey(User, related_name='sent_messages', on_delete=models.CASCADE)
-    content = models.TextField()
+    message_body = models.TextField()
     sent_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
